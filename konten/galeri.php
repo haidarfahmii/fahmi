410bc004cc0b4b3a58982f7eeb0a1be88d7bb8dd
haidarfahmii <59414625+haidarfahmii@users.noreply.github.com>
2020-01-10 14:54:27 +0700

    first commit

---
+++ konten/galeri.php
@@ -0,0 +1,6 @@
+<p> Ini Halaman Galeri</p>
+<p><img src="./images/uin.jfif" width="145" height="117"></p>
+<p><img src="./images/landmark_uin.jpg" width="145" height="117"></p>
+<p><img src="./images/perpus.jfif" width="145" height="117"></p>
+<p><img src="./images/fst.jfif" width="145" height="117"></p>
+    
---
+++ konten/jadwal.php
@@ -0,0 +1,64 @@
+<p>Ini Halaman Jadwal</p>
+<table width="400" border="1">
+  <tr>
+    <td width="22" bgcolor="#3399ff">No</td>
+    <td width="113" bgcolor="#3399ff">Mata Kuliah</td>
+    <td width="63" bgcolor="#3399ff">Hari</td>
+    <td width="76" bgcolor="#3399ff">Waktu</td>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Matematika Dasar 2</td>
+    <td>Senin</td>
+    <td>10.30-12.45</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Metode Numerik</td>
+    <td>Senin</td>
+    <td>13.45-16.15</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Organisasi Komputer</td>
+    <td>Selasa</td>
+    <td>13.00-14.40</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>Analisis Desain Sistem</td>
+    <td>Selasa</td>
+    <td>15.00-17.00</td>
+  </tr>
+  <tr>
+    <td>5</td>
+    <td>-</td>
+    <td>Libur</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td>6</td>
+    <td>Pemograman Web</td>
+    <td>Kamis</td>
+    <td>07.30-10.00</td>
+  </tr>
+  <tr>
+    <td>7</td>
+    <td>Komunikasi Data</td>
+    <td>Kamis</td>
+    <td>10.00-12.00</td>
+  </tr>
+  <tr>
+    <td>8</td>
+    <td>RPL</td>
+    <td>Kamis</td>
+    <td>13.00-14.40</td>
+  </tr>
+  <tr>
+    <td>9</td>
+    <td>Keamanan</td>
+    <td>Jum'at</td>
+    <td>14.00-15.30</td>
+  </tr>
+</table>
+  
